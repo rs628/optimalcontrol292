0c23e387f14e01b1ce51911e161b7888f68a4ab6
renu <rs628@snu.edu.in>
2019-09-12 16:02:26 +0530

    first commit

---
+++ example_2_9_2/dynamicCode/_3d2d6985c2654bee6358eb6b9168f1d0f063e779/codeStreamTemplate.C
@@ -0,0 +1,92 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) YEAR AUTHOR,AFFILIATION
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+#line 0 "/home/renu/example292/example_2_9_2/constant/yd.#codeStream"
+#include "fvCFD.H"
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_3d2d6985c2654bee6358eb6b9168f1d0f063e779
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 0 "/home/renu/example292/example_2_9_2/constant/yd.#codeStream"
+const IOdictionary& d = static_cast<const IOdictionary&>(dict);
+	    const fvMesh& mesh = refCast<const fvMesh>(d.db());
+
+	    scalarField yd(mesh.nCells(), 0.);
+		scalar pi = constant::mathematical::pi;
+
+	    forAll(yd, i)
+	    {
+		const scalar x = mesh.C()[i][0];
+		const scalar y = mesh.C()[i][1];
+
+		//yd[i] = x*y*sin(constant::mathematical::pi*x)*sin(constant::mathematical::pi*y);
+		//yd[i] = sin(pi*x)*sin(pi*y) + sin(8*pi*x)*sin(8*pi*y) ;
+		//Td[i] = sin(*x);   
+                   yd[i] = -(142.0/3.0)+12.0*(   Foam::pow(x-0.5,2.0)     +Foam::pow(y-0.5,2.0));  
+
+	    }
+
+	    yd.writeEntry("", os);
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
---
+++ laplaceAdjointFoam2/laplaceAdjointFoam2.C
@@ -0,0 +1,149 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    laplaceAdjointFoam
+
+Description
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// Main program:
+
+int main(int argc, char *argv[])
+{
+#include "setRootCase.H"
+#include "createTime.H"
+#include "createMesh.H"
+#include "createFields.H"
+
+    // Disable solvers performance output
+    lduMatrix::debug = 0;
+    solverPerformance::debug = 0;
+
+    // Cost function value
+    scalar J = 0;
+    scalar Jold = 0;
+    scalar Jk = 0;
+
+// Compute cost function value
+#include "costFunctionValue.H"
+
+    std::ofstream file("results.csv");
+    file << 0 << "," << J << "," << 0 << nl;
+
+    while (runTime.loop() && fabs(J - Jold) > tol)
+    {
+        // save old cost value
+        Jold = J;
+
+        // Primal equation
+        solve(fvm::laplacian(k, y) -y +  beta*u + f);
+
+        // Adjoint equation
+        solve(fvm::laplacian(k, p)-p + y - yd);
+
+        // Save current control
+        uk = u;
+
+        // calculate current cost
+         #include "costFunctionValue.H"
+          Jk = J;
+        bool alphaFound = false;
+
+        // calculate derivative^2 integrate((lambda*u + beta*p)^2 dv). Why??
+        scalar phip0 = gSum(volField * Foam::pow( uk.internalField() +  p.internalField(), 2));
+
+        while (!alphaFound)
+        {
+            u = uk - alpha *(uk + beta * p);
+
+            // truncate u for constrained control set
+            forAll(u,i)
+            {
+                u[i] = min(u[i], uMax[i]);
+                u[i] = max(u[i], uMin[i]);
+            }
+
+            u.correctBoundaryConditions();
+
+            // get new y
+            solve(fvm::laplacian(k, y) -y + beta * u + f);
+
+            // get new cost
+            
+        #include "costFunctionValue.H"
+  
+            if (J <= Jk - c1 * alpha * phip0)
+            {
+                Info << "alpha found, alpha = " << alpha << ", J = "   << J << ", phip0" << phip0 << endl;
+
+                alphaFound = true;
+            }
+            else
+            {
+                Info << "alpha NOT found, alpha = " << alpha << endl;
+                alpha = c2 * alpha;
+            }
+        Info << J<<endl; 
+        }
+
+        Info << "Iteration no. " << runTime.timeName() << " - "
+             << "Cost value " << J
+             << " - "
+             << "Cost variation" << fabs(J - Jold) << endl;
+
+        file << runTime.value() << "," << J << nl;
+
+        uc = -(beta/lambda)*p;
+        udiff = u - uc;
+
+        runTime.write();
+    }
+
+    file.close();
+
+    runTime++;
+    y.write();
+    p.write();
+    u.write();
+   // uc.write();
+    //udiff.write();
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info << nl << endl;
+    Info << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+         << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+         << nl << endl;
+
+    Info << "\nEnd\n"
+         << endl; 
+    return 0;
+}
+
+
+// ************************************************************************* //
